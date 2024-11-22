974b93a2c68dd39ced1790aa5fb7a6f1a5f5568d
timalo <44393499+timalo@users.noreply.github.com>
2024-11-22 14:29:16 +0200

    landingPage and productPage layout somewhat done. Should probably do product card component next

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import "./Layout.css";
+import "./Landing.css";
 
 const Landing = () => {
   return (
     <div className="content">
       <h1>Welcome to the Shopping page!</h1>
       <h2>Here you will find plenty of products at affordable prices</h2>
-      <Link to="/products">Products</Link>
+      <Link to="/products" className="landingLink">
+        Start shopping
+      </Link>
     </div>
   );
 };
--- src/Products.jsx
+++ src/Products.jsx
@@ -1,12 +1,12 @@
+import "./Layout.css";
+import "./Products.css";
+
 const Products = () => {
   return (
-    <div className="wholePage">
-      <div className="background">
-        <div className="contentDiv">
-          <h2>Product page</h2>
-          <p>On this page you should be able to find products.</p>
-        </div>
-      </div>
+    <div className="productContent">
+      <h2 className="centerTitle">Product page</h2>
+      <h3 className="centerTitle">Very good products here</h3>
+      <div className="productArea"></div>
     </div>
   );
 };
