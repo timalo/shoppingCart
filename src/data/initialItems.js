cd6aed380066469f83abaa0ba47d55dccd7672e5
timalo <44393499+timalo@users.noreply.github.com>
2024-11-25 14:47:53 +0200

    Managed to load product data into the page from mock initialItems.js. Shaping the products grid next

---
+++ src/data/initialItems.js
@@ -0,0 +1,26 @@
+const initialItems = [
+  {
+    id: 0,
+    name: "Item 1",
+    price: "10.99 €",
+    description: "This is a very good item",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  },
+  {
+    id: 1,
+    name: "Item 2",
+    price: "20.99 €",
+    description: "This is another very good item",
+    image:
+      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+  },
+  {
+    id: 2,
+    name: "Item 3",
+    price: "30.99 €",
+    description: "This is a very good item",
+    image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+  },
+];
+
+export default initialItems;
